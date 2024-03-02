99fa81e599ee7f247928be2cbf3c1517251c0012
Slinkter <luis.j.cueva@gmail.com>
2024-03-02 09:25:21 -0500

    update

--- src/components/product-title/index.jsx
+++ src/components/product-title/index.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../../store/slices/cart-slice";
 
 const ProductTile = ({ product }) => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state);
+
+  function handleAddToCart() {
+    dispatch(addToCart(product));
+  }
+  function handleRemoveFromCart() {
+    dispatch(removeFromCart(product.id));
+  }
+
   return (
     <div>
       <div className="group flex flex-col items-center border-2 border-red-900  gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
@@ -17,8 +29,17 @@ const ProductTile = ({ product }) => {
           </h1>
         </div>
         <div className="flex items-center justify-between w-full mt-5">
-          <button className="bg-red-900 text-white border-2 rounded-lg font-bold p-4">
-            Add To cart
+          <button
+            onClick={
+              cart.some((item) => item.id === product.id)
+                ? handleRemoveFromCart
+                : handleAddToCart
+            }
+            className="bg-red-900 text-white border-2 rounded-lg font-bold p-4"
+          >
+            {cart.some((item) => item.id === product?.id)
+              ? "remove from cart"
+              : "Add toCard"}
           </button>
         </div>
       </div>
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  const [totalCart, setTotalCart] = useState(0);
+  const { cart } = useSelector((state) => state);
+  console.log(cart);
+
+  useEffect(() => {
+    setTotalCart(cart.reduce((acc, curr) => acc + curr.price, 0));
+    return () => {};
+  }, []);
+
+  console.log(totalCart);
+
   return <div>Cart</div>;
 };
 
