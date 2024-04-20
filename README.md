18c009082f9a7c2b93bda5f278a2bb56fad20f7c
Slinkter <luis.j.cueva@gmail.com>
2024-04-20 12:07:37 -0500

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# React + Vite + tailwind
-
-npm i @reduxjs/toolkit react-redux react-icons react-router-dom react-loader-spinner
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import { Button } from "@material-tailwind/react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import Cart from "./pages/cart";
-import Header from "./components/header";
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-
-      <Routes>
-        <Route exac path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
