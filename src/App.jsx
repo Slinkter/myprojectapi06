c96169d2e155ddb0651aa55b4baf5d99e7ed295f
Slinkter <luis.j.cueva@gmail.com>
2024-09-20 12:30:58 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import { Button, Typography } from "@material-tailwind/react";
+import { Input } from "postcss";
 import React from "react";
 
 const App = () => {
     return (
         <>
-            <div className=" flex justify-center items-center h-[100vh]">
-                <h1>Project 06</h1>
-            </div>
+            <h1>hola</h1>
         </>
     );
 };
